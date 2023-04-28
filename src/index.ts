29e541461fe0c7a1326d81913324fa0ba85c335b
TheChoomba <choomba19@gmail.com>
2023-04-28 22:38:53 +0300

    3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
 import express, {Request, Response} from'express';
 
 const app = express();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3003;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World");
+const product  = [{id: 1, title: 'tomato'}, {id: 2, title: "orange"}]
+const addresses = [{id: 1, value: 'asdasda'}, {id: 2, value: 'asdqwewzxczx'}]
+
+app.get('/product', (req: Request, res: Response) => {
+    if(req.query.title) {
+        let serchString = req.query.title.toString();
+        res.send(product.filter(t => t.title?.indexOf(serchString) > -1))        
+    }
+    else { 
+        res.send(product);
+    }
+});
+
+app.get('/product/:id', (req: Request, res: Response) => {
+    let productShow = product.find(t => t.id === +req.params.id);
+    if(productShow) {
+        res.send(productShow);
+    }
+    else {
+        
+        res.send(404)
+    }
+});
+
+
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses);
+});
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    let id = addresses.find(t => t.id === +req.params.id);
+    if(id) {
+        res.send(id);
+    } 
+    else {
+        res.send(404);
+    }
 });
 
 app.listen(port, () => {
